0835f2d5bc5c7e3ab482743f7c26233ad6aaf1e6
jawadpathan1213-sys <jawadpathan1213@gmail.com>
2025-11-28 14:55:30 +0500

    Initial project

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className='overflow-x-hidden text-center flex justify-center '>
+    <div className='overflow-x-hidden text-center flex justify-center'>
       <div className='bg-[#0096c7] w-[85vw] my-[4vw] p-5 flex items-center flex-col gap-10 sm:gap-5 rounded-2xl'>
         <h1 className='text-[20px] font-bold mt-5'>Welcome to the Quiz App</h1>
         <img className='w-[300px]' src="/quez.png" alt="img" />
